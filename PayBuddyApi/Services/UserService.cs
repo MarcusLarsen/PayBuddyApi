b7e5c445c69d06e48e8c57c216b71cd4c3888a0d
Marcus Bisgaard Larsen <marc7511@elev.techcollege.dk>
2026-04-16 16:52:25 +0200

    update

--- PayBuddyApi/Services/UserService.cs
+++ PayBuddyApi/Services/UserService.cs
@@ -1,8 +1,18 @@
-﻿using PayBuddyApi.Interfaces;
+﻿using Microsoft.AspNetCore.Identity;
+using PayBuddyApi.Interfaces;
+using PayBuddyApi.Models;
 
 namespace PayBuddyApi.Services
 {
     public class UserService : IUserService
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _config;
+
+        public UserService(UserManager<AppUser> userManager, IConfiguration config)
+        {
+            _userManager = userManager;
+            _config = config;
+        }
     }
 }
